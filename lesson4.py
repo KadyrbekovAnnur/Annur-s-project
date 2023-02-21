afd5ad621dee7d0296a4056ee54c109ca746c331
Annur Kadyrbekov <pablo0808akd@gmail.com>
2023-02-21 18:58:12 +0600

    initial

---
+++ lesson4.py
@@ -0,0 +1,11 @@
+number = int(input('Введите число: '))
+sum_num = 0
+while number != 0:
+    last_num = number % 10
+    sum_num += last_num
+    if last_num == 5:
+        print('Обнаружен разрыв')
+        break
+    number //= 10
+    print('Текущая сумма цифр числа:', sum_num)
+print('\nИтоговая сумма цифр числа:', sum_num)
