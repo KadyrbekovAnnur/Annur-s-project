9106aa2a45df73f1e6a10b9681f9d58952e99697
Annur Kadyrbekov <pablo0808akd@gmail.com>
2023-02-23 12:19:03 +0600

    2 print added

--- lesson5.py
+++
@@ -1,9 +0,0 @@
-def factorial_num(num):
-    part_factorial = 1
-    sum_num = 0
-    for number in range(1, num + 1):
-        part_factorial *= number
-        sum_num += part_factorial
-    return sum_num
-n = int(input('Введите число: '))
-print(factorial_num(n))
